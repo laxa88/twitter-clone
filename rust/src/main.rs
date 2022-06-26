e2cfd0ecb86ea0bc8edfae7396d95836011b8aee
LWY <668915+laxa88@users.noreply.github.com>
2022-06-26 19:37:46 +0900

    feat(rust): add get_account_by_id endpoint

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,7 +2,10 @@
 
 // use postgres::{Client, NoTls};
 
+use rocket::serde::Serialize;
 use rocket::{fairing::AdHoc, serde::Deserialize, State};
+use rocket_db_pools::sqlx::{self, Row};
+use rocket_db_pools::{Connection, Database};
 
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
@@ -10,6 +13,10 @@ struct MyConfig {
     my_rocket_var_1: String,
     my_rocket_var_2: f32,
 }
+
+#[derive(Database)]
+#[database("myrustdb")]
+struct MyRustDb(sqlx::PgPool);
 
 #[macro_use]
 extern crate rocket;
@@ -36,6 +43,25 @@ async fn get_tweet_by_id(tweet_id: String, config: &State<MyConfig>) -> String {
     res
 }
 
+#[get("/db/<id>")]
+async fn get_account_by_id_1(mut db: Connection<MyRustDb>, id: i32) -> Option<String> {
+    println!("### get: {}", id);
+    sqlx::query("SELECT * FROM account WHERE id = $1")
+        .bind(id)
+        .fetch_one(&mut *db)
+        .await
+        .and_then(|r| {
+            let res = Account {
+                id: r.try_get(0)?,
+                email: r.try_get(1)?,
+                password: r.try_get(2)?,
+            };
+            println!("### got {:?}", res);
+            Ok(res.email)
+        })
+        .ok()
+}
+
 // #[rocket::main]
 // async fn main() -> Result<(), rocket::Error> {
 //     let _rocket = rocket::build().mount("/", routes![index]).launch().await?;
@@ -45,11 +71,16 @@ async fn get_tweet_by_id(tweet_id: String, config: &State<MyConfig>) -> String {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, hello, get_tweet_by_id])
+        .attach(MyRustDb::init())
+        .mount(
+            "/",
+            routes![index, hello, get_tweet_by_id, get_account_by_id_1],
+        )
         .attach(AdHoc::config::<MyConfig>())
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
+#[serde(crate = "rocket::serde")]
 struct Account {
     id: i32,
     email: String,
