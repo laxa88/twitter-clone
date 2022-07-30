b4ccba7b8e10e85999978d780e1c92d1ed004a2b
LWY <668915+laxa88@users.noreply.github.com>
2022-07-30 09:35:21 +0900

    feat(ci): update workflow

--- fe/src/lib/variables.ts
+++ fe/src/lib/variables.ts
@@ -1,6 +1,4 @@
 export const variables = {
-  // process.env injected via env-cmd
-  // Reference: https://joyofcode.xyz/sveltekit-environment-variables
   apiPort: import.meta.env.VITE_API_PORT || process.env.SECURE_API_PORT
 }
 
