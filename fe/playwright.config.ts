2b88855dccf672c2dacafffc27d45e9dd6adc883
LWY <668915+laxa88@users.noreply.github.com>
2022-08-06 10:01:34 +0900

    feat: update configs, docker-compose files

--- fe/playwright.config.ts
+++ fe/playwright.config.ts
@@ -1,28 +1,29 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import dotenv from 'dotenv';
 
+// Load variables from .env file
 dotenv.config();
 
-// console.log("#####", process.env.CI === '1')
-// console.log("#####", process.env.CI)
 console.log("#####", process.env.SECURE_API_HOST)
 console.log("#####", process.env.SECURE_API_PORT)
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		// Runs server (default port 4173)
+		// Builds and runs app (default served on port 4173)
 		command: 'npm run build && npm run preview -- --port 1122',
 
 		env: {
-			SECURE_API_HOST: process?.env.SECURE_API_HOST,
-			SECURE_API_PORT: process?.env.SECURE_API_PORT,
+			// Environment variables for connecting to Rust API
+			SECURE_API_HOST: process?.env.SECURE_API_HOST || '',
+			SECURE_API_PORT: process?.env.SECURE_API_PORT || '',
 		},
 
 		// Explicitly tell Playwright which port to connect to server
 		// url: 'http://localhost:1122'
-		port: 1122
-		// port: process.env.CI === '1' ? 1233 : 4173,
-		// timeout: 10000
+		port: 1122,
+
+		// Default is 30 or 60 seconds, but we don't wanna wait long
+		timeout: 10000,
 	},
 };
 
