de1f6cbc71fdd3bd9a6cafc1c83ae331d4860f5a
LWY <668915+laxa88@users.noreply.github.com>
2022-08-27 14:43:03 +0900

    feat(fe): refactor landing page

--- fe/src/routes/UserHome.svelte
+++ fe/src/routes/UserHome.svelte
@@ -1,1 +1,16 @@
-<main>You are logged in.</main>
+<script lang="ts">
+	import { storeJwt } from '$lib/store';
+	import { browser } from '$app/env';
+
+	const handleLogout = () => {
+		if (browser) {
+			window.localStorage.removeItem('jwt');
+			storeJwt.set('');
+		}
+	};
+</script>
+
+<main>
+	<h1>You are logged in.</h1>
+	<button on:click={handleLogout}>Logout</button>
+</main>
--- fe/src/routes/index.svelte
+++ fe/src/routes/index.svelte
@@ -3,6 +3,7 @@
 	import { browser } from '$app/env';
 	import Login from './Login.svelte';
 	import Register from './Register.svelte';
+	import UserHome from './UserHome.svelte';
 
 	if (browser) {
 		const jwt = window.localStorage.getItem('jwt');
@@ -16,24 +17,15 @@
 			jwt = value;
 		}
 	});
-
-	const handleLogout = () => {
-		if (browser) {
-			window.localStorage.removeItem('jwt');
-			storeJwt.set('');
-		}
-	};
 </script>
 
 <main>
-	<div>
-		{#if jwt}
-			<button on:click={handleLogout}>Logout</button>
-		{:else}
-			<Login />
-			<Register />
-		{/if}
-	</div>
+	{#if jwt}
+		<UserHome />
+	{:else}
+		<Login />
+		<Register />
+	{/if}
 </main>
 
 <!--
